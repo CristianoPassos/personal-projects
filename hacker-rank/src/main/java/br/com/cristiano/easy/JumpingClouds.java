35a22c3df3eae2690ab1fb40fd33cd2fab343c43
Cristiano Passos <cristiano.passos@auto1.com>
2019-04-05 11:54:07 +0200

    Hacker Rank exercises

---
+++ hacker-rank/src/main/java/br/com/cristiano/easy/JumpingClouds.java
@@ -0,0 +1,23 @@
+package br.com.cristiano.easy;
+
+public class JumpingClouds {
+
+    public static int jumpingOnClouds(int[] clouds) {
+        int thunderCloud = 1;
+        int jumps = 0;
+        int index = 0;
+        int indexLength = clouds.length - 1;
+
+        while (index < indexLength) {
+            int cloudPlusTwo = index + 2;
+            if (cloudPlusTwo < indexLength && clouds[cloudPlusTwo] == thunderCloud) {
+                index++;
+            } else {
+                index += 2;
+            }
+            jumps++;
+        }
+
+        return jumps;
+    }
+}
